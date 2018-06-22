43fc0d744289e208690ec2b265c0d5ff70afea31
张旭 <mob@chongzige.local>
2018-06-22 11:16:48 +0800

    Initial Commit

---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by mob on 2018/6/22.
+//  Copyright © 2018年 mob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by mob on 2018/6/22.
+//  Copyright © 2018年 mob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
